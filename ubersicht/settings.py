d4cbcf81bacae27110e976b4790d9ebd49416ea5
ICDCyborg <kobashinji@hotmail.com>
2023-09-04 11:19:05 +0900

    refactor: settings.py

--- ubersicht/settings.py
+++ ubersicht/settings.py
@@ -105,9 +105,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
