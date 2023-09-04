2894bb90437e5db50581e9d11f1ba704f92ff5d3
ICDCyborg <kobashinji@hotmail.com>
2023-09-04 11:09:06 +0900

    feat: index.html

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+class IndexView(TemplateView):
+    '''ウェルカムページを表示するビュー'''
+    template_name = 'index.html'
